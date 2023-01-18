55e530fab6fef39b804e7ff7ab7c78fa2090a9f7
aknibir-cse <aknibir.cse@gmail.com>
2023-01-18 21:38:57 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,314 @@
+<?php include 'header.php';?>
+  <!-- Bnr Header -->
+  <section class="main-banner">
+    <div class="tp-banner-container">
+      <div class="tp-banner">
+        <ul>
+          
+          <!-- SLIDE  -->
+          <li data-transition="random" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" > 
+            <!-- MAIN IMAGE --> 
+            <img src="images/hms.jpg"  alt="slider"  data-bgposition=" center center" data-bgfit="cover" data-bgrepeat="no-repeat"> 
+            
+            <!-- LAYER NR. 1 -->
+            <div class="tp-caption sfl tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-120" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300"
+                style="z-index: 5; font-size:50px; font-weight:500; color:black;  max-width: auto; max-height: auto; white-space: nowrap;">E-healthcare System</div>
+            
+            <!-- LAYER NR. 2 -->
+            <div class="tp-caption sfr tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-60" 
+                data-speed="800" 
+                data-start="1000" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                style="z-index: 6; font-size:40px; color:gray; font-weight:500; white-space: nowrap;">We care about your health </div>
+            
+            <!-- LAYER NR. 3 -->
+            <div class="tp-caption sfb tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="0" 
+                data-speed="800" 
+                data-start="1200" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="none" 
+                data-splitout="none" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                style="z-index: 7;  font-size:22px; color:gray; font-weight:500; max-width: auto; max-height: auto; white-space: nowrap;">Best Hospitality Services in your town</div>
+            
+            <!-- LAYER NR. 4 -->
+            <div class="tp-caption lfb tp-resizeme scroll" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="120"
+                data-speed="800" 
+                data-start="1300"
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 8;"><a href="patientappointment.php" class="btn">Book Now</a> </div>
+          </li>
+          
+          <!-- SLIDE  -->
+          <li data-transition="random" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" > 
+            <!-- MAIN IMAGE --> 
+            <img src="images/hms2.jpg"  alt="slider"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat"> 
+            
+            <!-- LAYER NR. 1 -->
+            <div class="tp-caption sfl tp-resizeme" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="-100" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300"
+                style="z-index: 5; font-size:40px; font-weight:500; color:#000;  max-width: auto; max-height: auto; white-space: nowrap;">Best Diagnostic Centre </div>
+            
+            <!-- LAYER NR. 2 -->
+            <div class="tp-caption sfr tp-resizeme" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="-40" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300"
+                style="z-index: 5; font-size:55px; font-weight:500; color:#000;  max-width: auto; max-height: auto; white-space: nowrap;">Care And Cure</div>
+            
+            <!-- LAYER NR. 3 -->
+            <div class="tp-caption sfb tp-resizeme" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="30" 
+                data-speed="800" 
+                data-start="1400" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="none" 
+                data-splitout="none" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                style="z-index: 7; font-size:16px; color:#000; font-weight:500; line-height:26px; max-width: auto; max-height: auto; white-space: nowrap;">Improved diagnostic performance and heightened satisfaction of patients <br> and physicians delight.</div>
+            
+            <!-- LAYER NR. 4 -->
+            <div class="tp-caption lfb tp-resizeme scroll" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="140"
+                data-speed="800" 
+                data-start="1600"
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 8;"><a href="contact.php" class="btn">CONTACT NOW</a> </div>
+          </li>
+          
+          <!-- SLIDE  -->
+          <li data-transition="random" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" > 
+            <!-- MAIN IMAGE --> 
+            <img src="images/hms3.jpg"  alt="slider"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat"> 
+            
+            <!-- LAYER NR. 2 -->
+            <div class="tp-caption sfb tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-80" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 6; font-size:40px; color:#000; font-weight:500; white-space: nowrap;"> Welcome To Our Research Center </div>
+            
+            <!-- LAYER NR. 3 -->
+            <div class="tp-caption sfb tp-resizeme text-center" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-10" 
+                data-speed="800" 
+                data-start="1000" 
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 7; font-size:20px; font-weight:500; line-height:26px; color:#000; max-width: auto; max-height: auto; white-space: nowrap;">We work in a friendly and efficient using the latest <br>
+              technologies and sharing our expertise.</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  
+  <!-- Content -->
+  <div id="content"> 
+    
+    <!-- Intro -->
+    <section class="p-t-b-150">
+      <div class="container">
+        <div class="intro-main">
+          <div class="row"> 
+            
+            <!-- Intro Detail -->
+            <div class="col-md-8">
+              <div class="text-sec">
+                <h5>Health Check Ups</h5>
+                <p>We offer a suite of products, custom-tailored to meet the needs of each hospital. Our integrated solutions enable hospitals to manage patient information, track financials, manage staff, and more. Our products are designed to be intuitive, easy to use and reliable, so that hospitals can focus their time and resources on delivering quality care to their patients.</p>
+                <ul class="row">
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
+                    <p>This application provides access to emergency medical services such as appointment booking services, doctors consultency services, and others medical services.The website also provides an online library of medical information and resources for doctors to use. 
+                    </p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
+                    <p>This application provides a variety of services to qualified doctors. The website also provides a secure portal for doctors to communicate with their patients and other healthcare professionals. Doctors can also access their patient’s medical history, review their lab results, and review their patient’s information. </p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> ONLINE APPOINTMENT</h6>
+                    <p>Online Appointment services allow users to book appointments with hospitals and healthcare professionals from the comfort of their homes. This Website provides comprehensive online appointment scheduling solutions for hospitals and clinics.</p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                    <p>Patients can access free medical counseling services from the comfort of their homes or from anywhere with an internet connection. Patients can book appointments with doctors and other healthcare professionals, get health advice, and receive personalized treatment plans.</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            
+            <!-- Intro Timing -->
+            <div class="col-md-4">
+              <div class="timing"> <i class="lnr lnr-clock"></i>
+                <ul>
+                  <li> Monday <span>8.00 - 16.00</span></li>
+                  <li> Tuesday <span>8.00 - 16.00</span></li>
+                  <li> Wednesday <span>8.00 - 16.00</span></li>
+                  <li> Thursday <span>8.00 - 16.00</span></li>
+                  <li> Friday <span>8.00 - 16.00</span></li>
+                  <li> Saturday <span>8.00 - 16.00</span></li>
+                  <li> Sunday <span>8.00 - 16.00</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+  
+
+    
+    <!-- DOCTOR LIST -->
+    <section class="p-t-b-150">
+      <div class="container"> 
+        
+        <!-- Heading -->
+        <div class="heading-block">
+          <h2>Our Services</h2>
+          <hr>
+          <span>Our E-healthcare System (HMS) is a comprehensive software solution that helps in streamlining the day-to-day activities of a hospital. It is designed to help healthcare providers, administrators, and healthcare professionals in managing their operations more efficiently. With HMS, hospitals can keep track of patient information, billing and accounts, store medical records, and schedule appointments. It also allows users to access medical reports, track patient vitals, and monitor patient health.</span> </div>
+        
+        <!-- Services -->
+        <div class="services">
+          <div class="row"> 
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-eye-2 icon"></i> </div>
+                <div class="media-body">
+                  <h6>Eye Specialist</h6>
+                  <p>This website provides a platform for Eye Specialists to communicate with their patients and answer any questions they may have.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-operating-room icon"></i> </div>
+                <div class="media-body">
+                  <h6>Operation Theater</h6>
+                  <p>This system allows for the tracking and management of operating room utilization. It also enables hospitals to set utilization goals and generate reports.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-icu-monitor icon"></i> </div>
+                <div class="media-body">
+                  <h6>ICU Department</h6>
+                  <p>With the help of this website, hospitals can keep track of the vital signs of their ICU patients in real-time, ensuring that they receive the best care possible. </p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-doctor icon"></i> </div>
+                <div class="media-body">
+                  <h6>Qualified Doctors</h6>
+                  <p>This website provides qualified doctors with a range of services designed to make their work easier and more efficient.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-heartbeat icon"></i> </div>
+                <div class="media-body">
+                  <h6>Heart Problems</h6>
+                  <p>This website provides comprehensive services for heart problems from diagnosis and treatment to recovery and rehabilitation.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-stomach-2 icon"></i> </div>
+                <div class="media-body">
+                  <h6>Stomach Problems</h6>
+                  <p>By this system, medical staffs are able to monitor patient meals and nutrition, as well as track any changes in the patient diet and lifestyle.</p>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+   
+    
+    
+  </div>
+  
+  <!-- Footer -->
+<?php include 'footer.php';?>
