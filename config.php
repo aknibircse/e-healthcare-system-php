00cc9e5ddde4f3368f045ae763dbc917b5077508
AK NIBIR <aknibir.cse@gmail.com>
2023-07-30 03:05:50 +0600

    deployed

--- config.php
+++ config.php
@@ -1,9 +1,8 @@
 <?php
-Sdb_host ="us-cdbr-east-06.cleardb.net";
-@db_user ="b565bab0e4ea98";
-$db_pass "cedb38e8";
+$db_host = "us-cdbr-east-06.cleardb.net";
+$db_user = "b565bab0e4ea98";
+$db_pass = "cedb38e8";
 $db_name ="heroku_0129eeda404b89b";
 
 $connect =mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die ("Database Connection Error.!!");
-
-
+?>
